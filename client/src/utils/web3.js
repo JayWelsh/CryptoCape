2098efe28343e4a9e8dac691efa80f3746b891a3
jay <thealchemicalopus@gmail.com>
2018-10-26 01:34:59 +0200

    changed to secure socket

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-const web3Client = new Web3(Web3.givenProvider || "ws://localhost:8546");
+const web3Client = new Web3(Web3.givenProvider || "wss://localhost:8546");
 
 export function isValidAddress(address){
     return web3Client.utils.isAddress(address);
