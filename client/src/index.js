8a9c350aaa46a673babd1e986dc6d55b589ec1b6
jay <thealchemicalopus@gmail.com>
2018-10-30 20:22:05 +0200

    Changed google analytics implementation

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 import { unregister } from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+//Google Analytics
+//Block with uBlock Origin if need be
+//This helps me get a rough estimate of site activity to help with scaling
+ReactGA.initialize('UA-128345594-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
