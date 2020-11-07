75e4ebf98dda083d6f19eb741a86d38ed47a9a49
Jay Welsh <thealchemicalopus@gmail.com>
2020-11-07 21:59:59 +0200

    fix(web3): due to compilation error, keep isAddress logic in codebase instead of using module

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -1,7 +1,12 @@
-import Web3 from 'web3';
-
-const web3Client = new Web3(Web3.givenProvider || "wss://localhost:8546");
-
 export function isValidAddress(address){
-    return web3Client.utils.isAddress(address);
+    const isAddress = function (address) {
+        // check if it has the basic requirements of an address
+        if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+            return false;
+            // If it's ALL lowercase or ALL upppercase
+        } else if (/^(0x|0X)?[0-9a-fA-F]{40}$/.test(address)) {
+            return true;
+        }
+    };
+    return isAddress(address);
 };
